cea35e56ad6abc8262277ebbc044601b84ed9ad2
Abhishek Aradhye <114404938+Abhishekaradhye@users.noreply.github.com>
2023-10-29 22:36:12 +0530

    Add files via upload

---
+++ Practice p21 classes.py
@@ -0,0 +1,18 @@
+class cricketers:
+    pass
+Virat = cricketers()
+Rohit = cricketers()
+
+Virat.fname = 'Abhishek'
+Virat.lname = 'Kohli'
+Virat.salary = '70000000'
+
+Rohit.fname = 'Rohit'
+Rohit.lname = 'Sharma'
+Rohit.salary = '70000000'
+
+print(Virat.lname,Virat.salary)
+print(Rohit.lname,Rohit.salary)
+
+
+
